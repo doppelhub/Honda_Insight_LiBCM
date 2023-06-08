409687076726d811926ebde531a956d7faa9688c
John Sullivan <buhtig.elt@xoxy.net>
2023-06-08 04:17:07 -0400

    set defaults in config.h

--- Firmware/MVP/config.h
+++ Firmware/MVP/config.h
@@ -19,12 +19,12 @@
 	//////////////////////////////
 
 	//choose your battery type:
-		//#define BATTERY_TYPE_5AhG3 //previously (incorrectly) referred to as "EHW5"
-		#define BATTERY_TYPE_47AhFoMoCo
+		#define BATTERY_TYPE_5AhG3 //previously (incorrectly) referred to as "EHW5"
+		//#define BATTERY_TYPE_47AhFoMoCo
 
 	//choose how many cells in series
-		//#define STACK_IS_48S
-		#define STACK_IS_60S
+		#define STACK_IS_48S
+		//#define STACK_IS_60S
 
 	//choose ONE of the following:
 		//#define SET_CURRENT_HACK_00 //OEM configuration (no current hack installed inside MCM)
@@ -43,8 +43,8 @@
 		//#define LIDISPLAY_CONNECTED  //Comment to disable all LiDisplay commands //JTS2doNow: mudder has not yet tested this code. Use at your own risk.
 
 	//choose which grid charger is installed
-		//#define GRIDCHARGER_IS_NOT_1500W
-		#define GRIDCHARGER_IS_1500W //Uncomment if using the optional "+15% SoC per hour" charger (UHP-1500-230) //sold only with FoMoCo Kits
+		#define GRIDCHARGER_IS_NOT_1500W
+		//#define GRIDCHARGER_IS_1500W //Uncomment if using the optional "+15% SoC per hour" charger (UHP-1500-230) //sold only with FoMoCo Kits
 
 	//////////////////////////////////////////////////////////////////
 
@@ -59,7 +59,7 @@
 
 	#define CELL_VMAX_REGEN                     42500 //42500 = 4.2500 volts
 	#define CELL_VMIN_ASSIST                    31900 //allows for ESR-based voltage drop
-	#define CELL_VMAX_GRIDCHARGER               31000 //3.9 volts is 75% SoC //other values: See SoC.cpp //MUST be less than 'CELL_VREST_85_PERCENT_SoC'
+	#define CELL_VMAX_GRIDCHARGER               39000 //3.9 volts is 75% SoC //other values: See SoC.cpp //MUST be less than 'CELL_VREST_85_PERCENT_SoC'
 	#define CELL_VMIN_GRIDCHARGER               30000 //grid charger will not charge severely empty cells
 	#define CELL_VMIN_KEYOFF                    CELL_VREST_10_PERCENT_SoC //when car is off, LiBCM turns off below this voltage  //JTS2doLater: Change to higher SoC
 	#define CELL_BALANCE_MIN_SoC                65    //when car is off, cell balancing is disabled below this percentage
