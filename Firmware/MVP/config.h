c0f272ec4831aa812be0d9bc3c79d555dbda421d
doppelhub <buhtig.elt@xoxy.net>
2023-12-15 18:22:32 -0500

    disabled config.h user config

--- Firmware/MVP/config.h
+++ Firmware/MVP/config.h
@@ -24,26 +24,26 @@
     //there are no default options because this firmware works with all LiBCM variants... you need to specify which hardware you have installed
 
     //choose your battery type:
-        #define BATTERY_TYPE_5AhG3 //if you're not sure, you probably have this battery
+        //#define BATTERY_TYPE_5AhG3 //if you're not sure, you probably have this battery
         //#define BATTERY_TYPE_47AhFoMoCo
 
     //choose how many cells are in series:
-        #define STACK_IS_48S //All 5AhG3 Kits & FoMoCo Kits with QTY4 modules
+        //#define STACK_IS_48S //All 5AhG3 Kits & FoMoCo Kits with QTY4 modules
         //#define STACK_IS_60S //FoMoCo Kits with QTY5 modules
 
     //choose which grid charger is installed
-        #define GRIDCHARGER_IS_NOT_1500W //All 5AhG3 Kits & 'standard' 47Ah FoMoCo Kits
+        //#define GRIDCHARGER_IS_NOT_1500W //All 5AhG3 Kits & 'standard' 47Ah FoMoCo Kits
         //#define GRIDCHARGER_IS_1500W //'faster' 47Ah FoMoCo Kits only
 
     //choose ONE of the following
     //must match actual "current hack" hardware configuration:
-        #define SET_CURRENT_HACK_40 //actually +45.8% //most LiBCM users installed this hardware option
+        //#define SET_CURRENT_HACK_40 //actually +45.8% //most LiBCM users installed this hardware option
         //#define SET_CURRENT_HACK_20 //actually +25.0%
         //#define SET_CURRENT_HACK_00 //OEM configuration (no current hack installed inside MCM)
 
     //choose which display to use
     //using both displays simultaneously could cause timing issues (FYI: the Serial Monitor prints '*' each time the loop period is violated)
-        #define LCD_4X20_CONNECTED  //display included with all LiBCM Kits
+        //#define LCD_4X20_CONNECTED  //display included with all LiBCM Kits
         //#define LIDISPLAY_CONNECTED //optional color touch screen display //JTS2doLater: mudder has not yet tested this code. Use at your own risk.
 
 
