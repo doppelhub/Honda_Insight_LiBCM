0b5b5e3858493350ac97b0867a62f9b859987c56
doppelhub <buhtig.elt@xoxy.net>
2024-11-01 18:52:32 -0400

    Increased FW expiration to 120 days

--- Firmware/firmwareLiBCM/src/LiDisplay.cpp
+++ Firmware/firmwareLiBCM/src/LiDisplay.cpp
@@ -1,4 +1,4 @@
-//Copyright 2021-2024(c) John Sullivan
+//
 //github.com/doppelhub/Honda_Insight_LiBCM
 
 //LiDisplay (HMI) Serial Functions
@@ -558,7 +558,7 @@ void LiDisplay_exitSettingsPage(void) {
 /////////////////////////////////////////////////////////////////////////////////////////
 
 void LiDisplay_checkFirmwareExpiration() {
-	if ((REQUIRED_FIRMWARE_UPDATE_PERIOD_HOURS - eeprom_uptimeStoredInEEPROM_hours_get()) <= 0)
+	if ((REQUIRED_FIRMWARE_UPDATE_PERIOD_HOURS - eeprom_hoursSinceLastFirmwareUpdate_get()) <= 0)
 	{
 		#ifdef LIDISPLAY_DEBUG_ENABLED
 			#undef LIDISPLAY_DEBUG_ENABLED
@@ -815,7 +815,7 @@ void LiDisplay_updateElement() {
 			switch (LiDisplayElementToUpdate)
 			{
 				case 0: LiDisplay_updateStringVal(1, "t1", 0, String(FW_VERSION)); break;
-				case 1: LiDisplay_updateStringVal(1, "t3", 0, String(REQUIRED_FIRMWARE_UPDATE_PERIOD_HOURS - eeprom_uptimeStoredInEEPROM_hours_get())); break;
+				case 1: LiDisplay_updateStringVal(1, "t3", 0, String(REQUIRED_FIRMWARE_UPDATE_PERIOD_HOURS - eeprom_hoursSinceLastFirmwareUpdate_get())); break;
 			}
 		break;
 
