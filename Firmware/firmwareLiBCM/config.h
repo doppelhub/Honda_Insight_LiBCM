2b4932080f2bf66b982c5157db9e6401c196a09f
doppelhub <buhtig.elt@xoxy.net>
2026-04-20 15:45:44 -0400

    Fixed negative temperature bug

--- Firmware/firmwareLiBCM/config.h
+++ Firmware/firmwareLiBCM/config.h
@@ -7,8 +7,8 @@
     #define config_h
     #include "src/libcm.h"
 
-    #define FW_VERSION "0.9.6a"
-    #define BUILD_DATE "2026APR03"
+    #define FW_VERSION "0.9.6b"
+    #define BUILD_DATE "2026APR20"
 
     //////////////////////////////////////////////////////////////////
 
